db4312b06039ed18f8d31f19ee5e435eea7847d9
yang <ywj9811@gmail.com>
2025-06-18 06:39:03 +0900

    feat : 고정 확장자 조회 dto

--- src/main/java/flow/work/dto/FixedExtensionResponse.java
+++ src/main/java/flow/work/dto/FixedExtensionResponse.java
@@ -5,12 +5,13 @@ import flow.work.entity.extension.FixedExtension;
 import java.util.List;
 
 public record FixedExtensionResponse(List<Extension> extensions) {
-    public record Extension(String extension, boolean isCheck) {}
+    public record Extension(String extension, boolean isCheck, long id) {}
 
     public static Extension from(FixedExtension entity) {
         return new Extension(
                 entity.getName().name().toLowerCase(),
-                entity.isCheck()
+                entity.isCheck(),
+                entity.getFixedExtensionId()
         );
     }
 }
