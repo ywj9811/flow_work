648a258402ec405166d9e8f1eac9a678d0cb6402
yang <ywj9811@gmail.com>
2025-06-19 06:51:32 +0900

    feat : 전역 예외 처리 추가

---
+++ src/main/java/flow/work/exception/GlobalApiExceptionHandler.java
@@ -0,0 +1,25 @@
+package flow.work.exception;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@Slf4j
+@RestControllerAdvice
+public class GlobalApiExceptionHandler {
+    private static final String LOG_FORMAT = "Error Message : {}";
+
+    @ExceptionHandler(ApplicationException.class)
+    public ResponseEntity<String> handleApplicationException(ApplicationException ex) {
+        log.error(LOG_FORMAT, ex.getMessage());
+        return ResponseEntity.internalServerError().body(ex.getMessage());
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleInternalServerException(Exception ex) {
+        log.error(LOG_FORMAT, ex.getStackTrace());
+        log.error("errorMessage : {}", ex.getMessage());
+        return ResponseEntity.internalServerError().body("실패하였습니다. 계속된 문제 발생시 문의 부탁드립니다.");
+    }
+}
